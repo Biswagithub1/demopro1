3ebd969440a1ae18d90c1c7b75891542a8e27648
Biswa <Biswa@Biswa-HP>
2024-07-20 09:06:29 +0530

    demo pro push

---
+++ demopro1/src/test/java/demopro1/ToOrgTest.java
@@ -0,0 +1,11 @@
+package demopro1;
+
+public class ToOrgTest {
+
+	public static void main(String[] args) {
+		System.out.println("Demo project1");
+		System.out.println("Automation Testing");
+
+	}
+
+}
